27f4edcd0ce3918bd6eaa4627020590228cc97c6
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-03-05 23:02:42 +0300

    feat: add total and positive feedback

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 3,
+    neutral: 5,
+    bad: 6,
   };
 
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100);
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const countTotalFeedback = this.countTotalFeedback();
+    const countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
     return (
       <div className="App">
         <h2>Please leave feedback</h2>
@@ -21,9 +36,11 @@ class App extends Component {
         <div>
           <h2>Statistics</h2>
           <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         </div>
       </div>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import s from "./Statistics.module.css";
 
-const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-    </div>
-  );
-};
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {total}</p>
+    <p>Positive feedback: {positivePercentage}%</p>
+  </div>
+);
 
 export default Statistics;
